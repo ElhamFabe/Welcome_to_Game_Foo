bd6085a80a04e8ec70f97c8a2ba12bdb9aae7719
Frank Buono <fbuonojr@fordham.edu>
2020-08-04 21:51:47 -0400

    added all game info to card

--- Assets/script.js
+++ Assets/script.js
@@ -25,68 +25,87 @@ $(document).ready(function () {
             if(response.name != null){
                 var nameEl = response.name;
                 console.log(nameEl);
-                var titleH = $("<h1>").text("Name: " + nameEl);
-                $("#game-result").append(titleH);
+                var titleH = $("<h3>").text("Name: " + nameEl);
+                $("#game-section").append(titleH);
             }
 
-            if(response.esrb_rating != null){
-                var ratingEl = response.esrb_rating;
-                console.log(ratingEl);
-            }
-
-            if(response.description_raw != null){
-                var descriptionEl = response.description_raw;   
-                console.log(descriptionEl);
-            }
-            
             if(response.background_image != null){
                 var imgEl = response.background_image;
                 console.log(imgEl);
+                var imgTag = $("<img>");
+                imgTag.attr("src", imgEl);
+                $("#game-section").append(imgTag);
             }
-            
-            var genreEl = [];
-            if(response.genres != null){
-                for(var i = 0; i < response.genres.length; i++){
-                    genreEl[i] = response.genres[i].name;
-                    console.log(genreEl[i]);
-                }
+
+            if(response.esrb_rating != null){
+                var ratingEl = response.esrb_rating.name;
+                console.log(ratingEl);
+                var ratingP = $("<p>").text("Rating: " + ratingEl);
+                $("#game-section").append(ratingP);
             }
+
+            if(response.description != null){
+                var descriptionEl = response.description_raw;   
+                console.log(descriptionEl);
+                var descriptionP = $("<p>").text("Description: " + descriptionEl)
+                $("#game-section").append(descriptionP);
+            }
+           
+            //need to work with array for multiple genres
+            // var genreEl = [];
+            // if(response.genres != null){
+            //     for(var i = 0; i < response.genres.length; i++){
+            //         genreEl[i] = response.genres[i].name;
+            //         console.log(genreEl[i]);
+            //     }
+            // }
             
+
             if(response.metacritic != null){
                 var metacriticEl = response.metacritic;
                 console.log(metacriticEl);
+                var metacriticP = $("<p>").text("Rating: " + metacriticEl);
+                $("#game-section").append(metacriticP);
             }
             
-            var developerEl = [];
-            if(response.developers != null){ 
-                for(var i = 0; i < response.developers.length; i++){    
-                    developerEl[i] = response.developers[i].name;
-                    console.log(developerEl[i]);
-                }
-            }
+            //need to work with array for developers
+            // var developerEl = [];
+            // if(response.developers != null){ 
+            //     for(var i = 0; i < response.developers.length; i++){    
+            //         developerEl[i] = response.developers[i].name;
+            //         console.log(developerEl[i]);
+            //     }
+            // }
             
-            var publisherEl = [];
-            if(response.publishers != null){
-                for(var i = 0; i < response.publishers.length; i++){ 
-                    publisherEl[i] = response.publishers[i].name;
-                    console.log(publisherEl[i]);
-                }
-            }
+            //need to work with array for publishers
+            // var publisherEl = [];
+            // if(response.publishers != null){
+            //     for(var i = 0; i < response.publishers.length; i++){ 
+            //         publisherEl[i] = response.publishers[i].name;
+            //         console.log(publisherEl[i]);
+            //     }
+            // }
             
             if(response.release != null){
                 var releaseEl = response.released;
                 console.log(releaseEl);
+                var releaseP = $("<p>").text("Release Date: " + releaseEl);
+                $("#game-section").append(releaseP);
             }
             
             if(response.website != null){
                 var websiteEl = response.website;
                 console.log(websiteEl);
+                var websiteP = $("<a>").attr("href", websiteEl)
+                websiteP.text(response.name + "'s Website");
+                $("#game-section").append(websiteP);
             }
             
-            if(response.platforms[0].platform.name != null){
-                var platformEl = response.platforms[0].platform.name;
-                console.log(platformEl);
-            }
+            //need to work with array for multi platform games
+            // if(response.platforms[0].platform.name != null){
+            //     var platformEl = response.platforms[0].platform.name;
+            //     console.log(platformEl);
+            // }
         });
     });
 });
