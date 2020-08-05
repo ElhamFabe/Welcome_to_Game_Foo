7b3860ed49cba11359cabd6047fc644188727607
Frank Buono <fbuonojr@fordham.edu>
2020-08-04 22:34:52 -0400

    start over button functionality

--- Assets/script.js
+++ Assets/script.js
@@ -109,6 +109,11 @@ $(document).ready(function () {
             // }
         });
     });
+
+    $("#clear-search").on("click", function (event) {
+        $("#game-section").empty();
+        document.getElementById("game-input").value = '';
+    });
 });
 
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
